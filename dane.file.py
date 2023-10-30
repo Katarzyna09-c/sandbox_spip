054fe08ad50e687a21d29b9f94d0ab5566e37b53
Katarzyna09-c <149382224+Katarzyna09-c@users.noreply.github.com>
2023-10-30 14:46:37 +0100

    Add functional

---
+++ dane.file.py
@@ -0,0 +1,13 @@
+users_list = [
+                   {"name":"Kasia" , "nick":"katarzyna","posts":1},
+                   {"name":"Mateusz" , "nick":"świetlik","posts":60},
+                   {"name":"Bartosz" , "nick":"Baran","posts":31231},
+                   {"name":"Kamil" , "nick":"Koc","posts":123},
+                   {"name":"Marek" , "nick":"Wisse","posts":5343},
+                   {"name":"Agata" , "nick":"Drzygało","posts":333},
+                   {"name":"Monika" , "nick":"Szczepaniuk","posts":4342}]
+
+print(f'Twój znajomy {zmienna_na_dane[0]["nick"]} opublikował {zmienna_na_dane[0]["posts"]}  !')
+print(f'Twój znajomy {zmienna_na_dane[1]["nick"]} opublikował {zmienna_na_dane[1]["posts"]}  !')
+print(f'Twój znajomy {zmienna_na_dane[2]["nick"]} opublikował {zmienna_na_dane[2]["posts"]}  !')
+print(f'Twój znajomy {zmienna_na_dane[3]["nick"]} opublikował {zmienna_na_dane[3]["posts"]}  !')
