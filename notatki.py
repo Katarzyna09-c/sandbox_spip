d0e0d5d454677a617c7a83a8bea34d66f9d1b2ad
Katarzyna09-c <149382224+Katarzyna09-c@users.noreply.github.com>
2023-11-27 13:55:08 +0100

    chore generowanie mapy

--- notatki.py
+++ notatki.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
-
 import requests
+import folium
 import re
+
 # pobranie strony internetowej
-nazwa_miejscowosci = 'Gdańsk'
+nazwa_miejscowosci = ['Gdańsk']
 def get_coordinates_of(city:str)->list[float,float]:
 
     adres_URL = f'https://pl.wikipedia.org/wiki/{city}'
@@ -22,6 +23,24 @@ def get_coordinates_of(city:str)->list[float,float]:
     # print(response_html_latitude[23:-7])
 
     #latitude = re.sub( "(\<).*?(\>)", repl='', string=response_html_latitude, count=0,flags=0)
-for item in nazwa_miejscowosci
-print(get_coordinates_of(nazwa_miejscowosci)
+#for item in nazwa_miejscowosci:
+  #  print(get_coordinates_of(item))
 
+# zwrócić mape z pinezką odnoszącą się do wskazanego na podstawie nazwy użytkownika podanej z klawiatury
+# zwróci mapę z wszystkimi użytkownikami z danej listy (znajomymi)
+### Rysowanie mapy
+city = get_coordinates_of(city ='Zamość')
+map = folium.Map(
+    location=[52.3, 21.0],    #gdzie mapa ma byc wycentrowana
+    tiles="OpenStreetMap",
+    zoom_start=7
+    )
+
+for item in nazwa_miejscowosci:
+    folium.Marker(
+        location=get_coordinates_of(city=item),
+        popup='GEOINFORMATYI RZADZI OHH YEAHHHHH !!!!!!!'
+
+    ).add_to(map)
+
+map.save('mapka.html')
